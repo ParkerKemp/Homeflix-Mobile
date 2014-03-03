fc61ff8ddff5ce508ab6cecc3ccab67adefc15a4
Parker Kemp <charles.kemp702@topper.wku.edu>
2014-03-02 19:09:46 -0600

    Created some text fields in Activity Editor, changed the value of one programmatically.

--- src/com/thundercats/homeflix_mobile/MainActivity.java
+++ src/com/thundercats/homeflix_mobile/MainActivity.java
@@ -3,13 +3,42 @@ package com.thundercats.homeflix_mobile;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//Called at the very beginning
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView text = (TextView) findViewById(R.id.textView2);
+		text.setText("Yo");
+	}
+	
+	@Override
+	protected void onStart(){
+		//Called after onCreate, and also after onRestart if the user is returning to it after onStop
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume(){
+		//Called after onStart, and also after onPause when the user returns to it
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause(){
+		//Called when another activity comes into the foreground, but this one is still partially visible
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop(){
+		//Called when the activity is no longer visible to the user
+		super.onStop();
 	}
 
 	@Override
@@ -19,4 +48,9 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onDestroy(){
+		//Exit app
+		super.onDestroy();
+	}
 }

f26852ce6dee63c4fcf3c80c10ddb62b33dcdcbf
Parker Kemp <charles.kemp702@topper.wku.edu>
2014-05-01 21:02:01 -0500

    Keep track of place in video when screen is rotated (needs more work).

--- src/com/thundercats/homeflix_mobile/VideoStream.java
+++ src/com/thundercats/homeflix_mobile/VideoStream.java
@@ -2,6 +2,7 @@ package com.thundercats.homeflix_mobile;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,19 +10,33 @@ import android.widget.VideoView;
 import android.widget.MediaController;
 
 public class VideoStream extends Activity{
-
+	VideoView videoView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
 		setContentView(R.layout.activity_video);
 		String url = getIntent().getStringExtra("com.thundercats.homeflix_mobile.streamurl");
 		System.out.println("New activity got: " + url);
-		VideoView videoView = (VideoView) this.findViewById(R.id.videoView);
-		videoView.setVideoURI(Uri.parse(url));        
+		int timestamp = prefs.getInt("timestamp", 0);
+		System.out.println(timestamp);
+		
+		videoView = (VideoView) this.findViewById(R.id.videoView);
+		videoView.setVideoURI(Uri.parse(url));
 		videoView.setMediaController(new MediaController(this));
-		videoView.seekTo(2000);
 		videoView.requestFocus();
+		videoView.seekTo(timestamp);
 		videoView.start();
+	}
+	
+	@Override
+	public void onPause(){
+		super.onPause();
+		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt("timestamp", videoView.getCurrentPosition());
+		editor.commit();
 	}
 }
 /*
